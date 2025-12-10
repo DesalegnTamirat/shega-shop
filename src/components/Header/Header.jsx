6a5219b1ec9b2b66df69300b605e9f26a48da7b6
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2025-12-11 00:09:53 +0300

    add styling for header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,53 +1,59 @@
 import { SlLocationPin } from "react-icons/sl";
 import logo from "../../assets/images/logo.png";
 import englishLogo from "../../assets/images/USA-EN-LOGO.svg";
-import { FaSearch } from "react-icons/fa";
+import styles from "./header.module.css";
+import { BiCart, BiSearch } from "react-icons/bi";
+import LowerHeader from "./LowerHeader";
 
 function Header() {
   return (
     <header>
-      <div>
-        <div>
+      <div className={styles.header__container}>
+        <div className={styles.logo__container}>
           <a href="#">
             <img src={logo} alt="amazon logo" />
           </a>
-          <span>
-            <SlLocationPin />
-          </span>
-          <div>
-            <p>Delivered to</p>
-            <span>Ethiopia</span>
+          <div className={styles.delivery}>
+            <span>
+              <SlLocationPin />
+            </span>
+            <div>
+              <p>Delivered to</p>
+              <span>Ethiopia</span>
+            </div>
           </div>
         </div>
-        <div>
+        <div className={styles.search}>
           <select name="" id="">
             <option value="">All</option>
           </select>
           <input type="text" placeholder="search product" />
-          <FaSearch />
+          <BiSearch size={25} />
         </div>
-        <div>
-          <div>
+        <div className={styles.order__container}>
+          <a className={styles.language}>
             <img src={englishLogo} alt="USA" />
             <select>
               <option value="">EN</option>
             </select>
-          </div>
-          <a href="">
+          </a>
+          <a>
             <div>
               <p>Sign In</p>
               <span>Account & Lists</span>
             </div>
           </a>
-          <a href="">
+          <a>
             <p>Returns</p>
             <span>& Orders</span>
           </a>
-          <a href="">
+          <a className={styles.cart}>
+            <BiCart size={35} />
             <span>0</span>
           </a>
         </div>
       </div>
+      <LowerHeader />
     </header>
   );
 }
